b0907a6e1f47f01ddf9c499a3ba7403b78ac78b8
Pierre Chanial <pchanial@sappcn129.extra.cea.fr>
2010-02-25 16:23:42 +0100

    test_tamasis.py:    - make use of scipy.sparse.linalg.cgs (PCG)    - restrict solving the map to a given mask (pixels with weights>1)    - use preconditioner M = diag(1/weights)    - don't use Masking class for now, putmask is way too inefficient

--- test_tamasis.py
+++ test_tamasis.py
@@ -1,32 +1,48 @@
-from tamasis import Identity, CompressionAverage, PacsProjectionSharpEdges, PacsMultiplexing, Masking, PacsObservation, PacsSimulation, Map, Tod, hcss_photproject
+from tamasis import Identity, CompressionAverage, PacsProjectionSharpEdges, PacsMultiplexing, Masking, PacsObservation, PacsSimulation, Map, Tod, hcss_photproject, LeastSquareMatvec, Matmat, PcgCallback
 from copy import copy
+import numpy
+import scipy
+from scipy.sparse import dia_matrix
+from scipy.sparse.linalg import LinearOperator, cgs
 
 datadir = '/home/pchanial/work/pacs/data/'
-pacs = PacsObservation(filename=datadir+'transparent/NGC6946/1342184520_blue', \
-                       first=20000,                 \
-                       last=86000,                  \
-                       resolution=3.,               \
-                       fine_sampling_factor=1,      \
-                       keep_bad_detectors=False,    \
+pacs = PacsObservation(filename=datadir+'transparent/NGC6946/1342184520_blue',
+                       first=20000,
+                       last=86000,
+                       resolution=3.,
+                       fine_sampling_factor=1,
+                       keep_bad_detectors=False,
                        npixels_per_sample=6)
+
 tod = pacs.get_tod()
 
 telescope    = Identity('Telescope PSF')
 projection   = PacsProjectionSharpEdges(pacs)
-#multiplexing = PacsMultiplexing(pacs)
 multiplexing = CompressionAverage(pacs.fine_sampling_factor, 'Multiplexing')
 crosstalk    = Identity('Crosstalk')
 compression  = CompressionAverage(pacs.compression_factor)
 masking      = Masking(tod.mask)
 
-model = masking * compression * crosstalk * multiplexing * projection * telescope
+model = crosstalk * multiplexing * projection * telescope
 print model
 
-mymap = copy(model.transpose(tod))
+tod = pacs.get_tod()
+backmap = copy(model.transpose(tod))
+backmap.mask=numpy.ma.nomask
 tod[:] = 1.
-weights = model.transpose(tod)
-mymap /= weights
-mymap.writefits('/home/pchanial/work/tamasis/ngc6946.fits')
+weights = copy(model.transpose(tod))
+map0 = Map(backmap / weights, mask=weights <= 1)
+backmap.mask = map0.mask
+
+shape = 2*(map0.count(),)
+matvec = LeastSquareMatvec(model, map0.mask)
+operator = LinearOperator(matvec=matvec, dtype=numpy.float64, shape=shape)
+b  = backmap.compressed()
+x0 = map0.compressed()
+M  = dia_matrix(((1./weights)[weights > 1], 0), shape=shape)
+solution, nit = cgs(operator, b, x0=x0, M=M, tol=1.e-4, maxiter=200, callback=PcgCallback())
+mymap = copy(map0)
+mymap[mymap.mask == False] = solution
 
 #map = hcss_photproject(pacs)
 
