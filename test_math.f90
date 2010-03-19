25431dcbd2d810ae179a3266c4d0754ef4731726
Pierre Chanial <pchanial@sappcn129.extra.cea.fr>
2010-03-19 11:02:04 +0100

    test_math.f90: update

--- test_math.f90
+++ test_math.f90
@@ -1,7 +1,7 @@
 program test_math
 
     use precision, only : p
-    use module_math, only : moment, linspace, logspace, test_real_eq
+    use module_math, only : NaN, moment, linspace, logspace, nint_down, nint_up, test_real_eq
     implicit none
 
     real(kind=p), parameter :: sample(5) = [0.204803265708662_p,           &
@@ -10,13 +10,9 @@ program test_math
     real(kind=p), parameter :: mresults(6) = [0.617858700525270_p,         &
             0.133548079242052_p,-0.285146600004998_p,-2.246479349723560_p, &
             0.365442306311204_p,0.319514776903196_p]
-    real(kind=p) :: mean, variance, skewness, kurtosis, stddev, meandev, nan, zero
+    real(kind=p) :: mean, variance, skewness, kurtosis, stddev, meandev
     real(kind=p), allocatable :: x(:)
     integer                   :: i
-
-    !XXX ISO_IEEE...
-    zero = 0._p
-    NaN  = 0._p / zero
 
     call moment(sample, mean, variance, skewness, kurtosis, stddev=stddev, &
                 meandev=meandev)
@@ -58,6 +54,15 @@ program test_math
     if (any(.not. test_real_eq(x, [1._p, 1.41421356237309_p, 2._p], 14))) then
         stop 'FAILED: logspace'
     end if
+    deallocate(x)
+
+    allocate(x(8))
+    x = [0._p,0.2_p,0.5_p,0.8_p,1._p,1.2_p,1.5_p,1.7_p]
+    if (any(nint_down( x) /= [0,0,0,1,1,1,1,2])) stop 'FAILED: nint_down 1'
+    if (any(nint_down(-x) /=-[0,0,1,1,1,1,2,2])) stop 'FAILED: nint_down 2'
+    if (any(nint_up  ( x) /= [0,0,1,1,1,1,2,2])) stop 'FAILED: nint_up 1'
+    if (any(nint_up  (-x) /=-[0,0,0,1,1,1,1,2])) stop 'FAILED: nint_up 2'
+    deallocate(x)
 
     stop 'OK.'
 
