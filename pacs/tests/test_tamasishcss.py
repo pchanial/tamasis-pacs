fc9f3d16e9864cf7cb57688c714ad74dfe044cb7
Pierre CHANIAL <pchanial@sapherschel4.extra.cea.fr>
2010-07-09 16:12:31 +0200

    tamasishcss:     - tamasis -> tamasishcss     - update test file

--- pacs/tests/test_tamasishcss.py
+++ pacs/tests/test_tamasishcss.py
@@ -1,11 +1,11 @@
 import os
-from tamasis import *
+from tamasishcss import *
 
 fa = FitsArchive(reader=FitsArchive.HCSS_READER)
 path = '/mnt/herschel1/mapmaking/data/pacs/M81prime/'
-frames = (fa.load(path+'1342186085_red_PreparedFrames.fits'), \
-          fa.load(path+'1342186086_red_PreparedFrames.fits'))
-#frames = fa.load(tamasis_dir+'tests/frames_blue.fits')
+#frames = (fa.load(path+'1342186085_red_PreparedFrames.fits'), \
+#          fa.load(path+'1342186086_red_PreparedFrames.fits'))
+frames = fa.load(tamasis_dir+'tests/frames_blue.fits')
 map = tamasisPhotProject(frames, \
                          updateFrames=True, \
                          deglitching='l2mad', \
@@ -13,5 +13,6 @@ map = tamasisPhotProject(frames, \
                          flatfielding=True, \
                          framePolicyTurnaround='keep', \
                          npixelsPerSample=6, \
-                         ds9=True)
+                         ds9=False)
+Display(map)
 print 'Done.'
