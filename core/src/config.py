8afb76b95ec82cde7f3b3f3e785a0a5acbac3260
Pierre Chanial <pierre.chanial@cea.fr>
2010-11-01 13:10:42 +0100

    Version 1.8.0

--- core/src/config.py
+++ core/src/config.py
@@ -3,7 +3,7 @@ __verbose__ = False
 tamasis_dir = os.path.dirname(__file__) + '/../' if os.path.dirname(__file__) != '' else '../'
 del os
 
-__version_info__ = (1, 7, 0)
+__version_info__ = (1, 8, 0)
 __version__ = '.'.join((str(i) for i in __version_info__))
 
 __all__ = [ 'get_default_dtype', 'get_default_dtype_complex', 'get_default_dtype_float', 'tamasis_dir', '__verbose__', 
