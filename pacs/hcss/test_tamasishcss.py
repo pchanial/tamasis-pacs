b2cd3014654872d35c131e8806d469323a9291b3
Pierre Chanial <pierre.chanial@cea.fr>
2010-11-02 23:36:30 +0100

    pacs hcss:     - implement new task TamasisPrepareFramesTask     - mv other tasks from __init__.py to processing.py

--- pacs/hcss/test_tamasishcss.py
+++ pacs/hcss/test_tamasishcss.py
@@ -3,13 +3,13 @@ from tamasishcss import *
 
 fa = FitsArchive(reader=FitsArchive.HCSS_READER)
 frames = fa.load(tamasis_dir+'pacs/test/data/frames_blue.fits')
-map = tamasisPhotProject(frames, \
-                         updateFrames=True, \
-                         deglitching='l2mad', \
-                         medianFiltering=10000, \
-                         flatfielding=True, \
-                         policyTurnaround='keep', \
-                         npixelsPerSample=6, \
+map = tamasisPhotProject(frames,
+                         updateFrames=True,
+                         deglitching='l2mad',
+                         medianFiltering=10000,
+                         flatfielding=True,
+                         policyTurnaround='keep',
+                         npixelsPerSample=6,
                          ds9=False)
 Display(map)
 print 'Done.'
