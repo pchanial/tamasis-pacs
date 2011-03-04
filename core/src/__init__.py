0e8397ee7382799f881933f0905a4503775da2c5
Pierre Chanial <pierre.chanial@cea.fr>
2011-03-04 09:55:35 +0100

    __init__.py: do not import subpackages in tamasis' __all__

--- core/src/__init__.py
+++ core/src/__init__.py
@@ -4,3 +4,5 @@ from .pacs import *
 
 del acquisitionmodels, datatypes, mappers, numpyutils, observations, processing, quantity, tamasisfortran, utils
 del core, madcap, pacs
+
+__all__ = [ f for f in dir() if f[0] != '_' and f not in ('mpiutils', 'stringutils', 'tmf', 'var', 'wcsutils')]
--- core/test/test_tamasis.py
+++ core/test/test_tamasis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tamasis
 from tamasis import *
 
 class TestFailure(Exception):
@@ -6,17 +7,24 @@ class TestFailure(Exception):
 
 a = np.ones((10,10))
 b = a.copy()
-
-tmf.add_inplace(a.ravel(), b.ravel())
+tamasis.tmf.add_inplace(a.T, b.T)
 if any_neq(a, 2): raise TestFailure()
 
 b[:] = -1
-tmf.subtract_inplace(a.ravel(), b.ravel())
+tamasis.tmf.subtract_inplace(a.T, b.T)
 if any_neq(a, 3): raise TestFailure()
 
 b[:] = 2
-tmf.multiply_inplace(a.ravel(), b.ravel())
+tamasis.tmf.multiply_inplace(a.T, b.T)
 if any_neq(a, 6): raise TestFailure()
 
-tmf.divide_inplace(a.ravel(), b.ravel())
+tamasis.tmf.divide_inplace(a.T, b.T)
 if any_neq(a, 3): raise TestFailure()
+
+a1 = np.ones((2,3,4,5))
+a2 = np.ones((2,3,4,5))
+b = np.arange(2*3*4, dtype=tamasis.var.FLOAT_DTYPE).reshape((2,3,4))
+tamasis.tmf.multiply_inplace(a1.T, b.T)
+a2.T[:] *= b.T
+
+if any_neq(a1, a2): raise TestFailure()
