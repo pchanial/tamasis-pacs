55b083e28d7d90fac49513daa9eaf7bebb6d7495
Pierre Chanial <pierre.chanial@cea.fr>
2011-04-26 00:22:04 +0200

    tamasis module: add back utils sub package

--- core/src/__init__.py
+++ core/src/__init__.py
@@ -5,7 +5,7 @@ from .core import *
 from .madcap import *
 from .pacs import *
 
-del acquisitionmodels, datatypes, mappers, numpyutils, observations, processing, quantity, tamasisfortran, utils
+del acquisitionmodels, datatypes, mappers, numpyutils, observations, processing, quantity, tamasisfortran
 del core, madcap, pacs
 
 __all__ = [ f for f in dir() if f[0] != '_' and f not in ('mpiutils', 'stringutils', 'tmf', 'var', 'wcsutils')]
