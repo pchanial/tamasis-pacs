56d55358e651923272aed938ddca334c66b3529f
Pierre Chanial <pierre.chanial@cea.fr>
2011-06-07 08:55:11 +0200

    __init__.py: add debugging listener

--- core/src/__init__.py
+++ core/src/__init__.py
@@ -1,6 +1,12 @@
 # Copyrights 2010-2011 Pierre Chanial
 # All rights reserved
 #
+try:
+    import pydebug
+    pydebug.listen()
+    del pydebug
+except:
+    pass
 from .core import *
 from .madcap import *
 from .pacs import *
@@ -9,3 +15,4 @@ del acquisitionmodels, datatypes, mappers, numpyutils, observations, processing,
 del core, madcap, pacs
 
 __all__ = [ f for f in dir() if f[0] != '_' and f not in ('mpiutils', 'stringutils', 'tmf', 'var', 'wcsutils')]
+
