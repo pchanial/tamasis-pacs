e317017e9a90a26ef5d73c3184a2b6decc277658
Pierre Chanial <pierre.chanial@cea.fr>
2011-12-19 11:49:11 +0100

    Update to nosetests.

--- core/test/test_mpifits.py
+++ core/test/test_mpifits.py
@@ -1,51 +1,41 @@
 from __future__ import division
 
 import os
-import pyfits
-import tamasis
+import numpy as np
+from glob import glob
 from mpi4py import MPI
-from tamasis import *
+from tamasis import Map
+from tamasis.numpyutils import assert_all_eq
 from uuid import uuid1
-
-class TestFailure(Exception): pass
-
-tamasis.var.verbose=True
 
 rank = MPI.COMM_WORLD.Get_rank()
 size = MPI.COMM_WORLD.Get_size()
+path_data = os.path.dirname(__file__) + '/data/'
+id = MPI.COMM_WORLD.bcast(str(uuid1()))
 
-filename = os.path.dirname(__file__) + '/data/' + 'frames_blue_map_naive.fits'
-map_ref = pyfits.open(filename)[0].data
+def test():
+    filename = path_data + 'frames_blue_map_naive.fits'
+    ref = Map(filename, comm=MPI.COMM_SELF)
+    local = Map(filename, comm=MPI.COMM_WORLD)
+    local2 = ref.tolocal()
 
-ref = Map(filename, comm=MPI.COMM_SELF)
-local = Map(filename, comm=MPI.COMM_WORLD)
-local2 = ref.tolocal()
+    assert local.shape[0] == int(np.ceil(ref.shape[0] / size))
+    assert_all_eq(local, local2)
 
-if local.shape[0] != int(np.ceil(ref.shape[0] / size)): raise TestFailure()
-if any_neq(local, local2): raise TestFailure()
+    ref2 = local.toglobal()
+    assert_all_eq(ref, ref2)
 
-ref2 = local.toglobal()
-if any_neq(ref, ref2): raise TestFailure()
-
-if rank == 0:
-    filename2 = 'test-'+str(uuid1())+'.fits'
-else:
-    filename2 = None
-filename2 = MPI.COMM_WORLD.bcast(filename2)
-
-try:
+    filename2 = 'test-' + id + '.fits'
     local.save(filename2)
-    ref2 = Map(filename2, comm=MPI.COMM_SELF)
-    local2 = Map(filename2, comm=MPI.COMM_WORLD)
-finally:
-    try:
-        os.remove(filename2)
-    except:
-        pass
+    ref3 = Map(filename2, comm=MPI.COMM_SELF)
+    local3 = Map(filename2, comm=MPI.COMM_WORLD)
 
-if any_neq(ref, ref2):
-    raise TestFailure()
+    assert_all_eq(local, local3)
+    assert_all_eq(ref, ref3)
 
-if any_neq(local, local2):
-    raise TestFailure()
-
+def teardown():
+    if rank > 0:
+        return
+    files = glob('*' + id + '.fits')
+    for f in files:
+        os.remove(f)
