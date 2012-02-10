33d4cb5b0966f33ef2b0e0f61701b99251c657bc
Pierre Chanial <pierre.chanial@cea.fr>
2012-02-10 13:07:20 +0100

    Copy over some routines from pyoperators.operators_mpi.

--- core/src/mpiutils.py
+++ core/src/mpiutils.py
@@ -7,14 +7,55 @@ import numpy as np
 import os
 import pyfits
 import sys
-import tamasisfortran as tmf
 
-from pyoperators.operators_mpi import distribute_shape, distribute_slice
-from pyoperators.utils import openmp_num_threads, strshape
+from pyoperators.utils import openmp_num_threads, strshape, tointtuple
 from . import MPI
 from .wcsutils import create_fitsheader
 
 __all__ = []
+
+
+
+def distribute_shape(shape, rank=None, size=None, comm=None):
+    """
+    Return the shape of a local array given the shape of a global array,
+    according to the rank of the MPI job, The load is distributed along
+    the first dimension.
+    """
+    if rank is None or size is None:
+        comm = comm or MPI.COMM_WORLD
+    if size is None:
+        size = comm.size
+    if rank is None:
+        rank = comm.rank
+
+    shape = tointtuple(shape)
+    if len(shape) == 0:
+        if size > 1:
+            raise ValueError('It is ambiguous to split a scalar across processe'
+                             's.')
+        return ()
+    nglobal = shape[0]
+    nlocal = nglobal // size + ((nglobal % size) > rank)
+    return (nlocal,) + tuple(shape[1:])
+
+
+def distribute_slice(nglobal, rank=None, size=None, comm=None):
+    """
+    Given a number of ordered global work items, return the slice that brackets
+    the items distributed to a local MPI job.
+    """
+    if rank is None or size is None:
+        comm = comm or MPI.COMM_WORLD
+    if size is None:
+        size = comm.size
+    if rank is None:
+        rank = comm.rank
+    nlocal = nglobal // size + ((nglobal % size) > rank)
+    start = nglobal // size * rank + min(rank, nglobal % size)
+    stop = start + nlocal
+    return slice(start, stop)
+
 
 def distribute_observation(detectors, observations, rank=None, comm=None):
 
