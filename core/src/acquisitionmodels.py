Fix normalisation in InvNttUncorrelatedOperator and swap right and left. Write different routines for inplace and outplace case. Don't modify mask in PadOperator.

core/src/acquisitionmodels.py
@@ -409,8 +409,8 @@ class InvNttUncorrelatedOperator(Operator):
         array = np.empty(filter_length, dtype=var.FLOAT_DTYPE)
         self.fft_filter = fft_filter
         self.filter_length = filter_length
-        self.left = filter_length - nsamples - ncorrelations
-        self.right = ncorrelations
+        self.left = ncorrelations
+        self.right = filter_length - nsamples - ncorrelations
         self.fftw_flags = fftw_flags
         self.fplan = fftw3.Plan(array, direction='forward', flags=fftw_flags,
             realtypes=['halfcomplex r2c'], nthreads=1)
@@ -420,9 +420,16 @@ class InvNttUncorrelatedOperator(Operator):
         
     def direct(self, input, output):
         input_, ishape, istride = _ravel_strided(input)
+        if self.same_data(input, output):
+            tmf.operators.invntt_uncorrelated_inplace(input_, ishape[1],
+                istride, self.fft_filter.T, self.fplan._get_parameter(),
+                self.bplan._get_parameter(), self.left, self.right)
+            return
         output_, oshape, ostride = _ravel_strided(output)
-        tmf.invntt_uncorrelated(input_, ishape[1], istride, output_, ostride, self.fft_filter.T, self.fplan._get_parameter(), self.bplan._get_parameter(), self.left, self.right)
-       
+        tmf.operators.invntt_uncorrelated_outplace(input_, ishape[1], istride,
+            output_, ostride, self.fft_filter.T, self.fplan._get_parameter(),
+            self.bplan._get_parameter(), self.left, self.right)
+
 
 @real
 @symmetric
@@ -458,16 +465,12 @@ class PadOperator(Operator):
         Operator.__init__(self, **keywords)        
    
     def direct(self, input, output):
-        if hasattr(output, 'mask'):
-            output.mask = None
         right = -self.right if self.right != 0 else output.shape[-1]
         output[...,:self.left] = 0
         output[...,self.left:right] = input
         output[...,right:] = 0
    
     def transpose(self, input, output):
-        if hasattr(output, 'mask'):
-            output.mask = None
         right = -self.right if self.right != 0 else input.shape[-1]
         output[...] = input[...,self.left:right]
 

core/test/test_acquisitionmodels.py
@@ -5,7 +5,7 @@ from pyoperators import Operator, AdditionOperator, CompositionOperator, BlockDi
 from pyoperators.utils import isscalar
 from pyoperators.utils.testing import assert_is_instance
 from numpy.testing import assert_equal, assert_almost_equal, assert_raises
-from tamasis.acquisitionmodels import BlackBodyOperator, ConvolutionOperator, CompressionAverageOperator, DdTddOperator, DiscreteDifferenceOperator, DownSamplingOperator, FftHalfComplexOperator, IdentityOperator, MaskOperator, PackOperator, PadOperator, ConvolutionTruncatedExponentialOperator, RollOperator, ShiftOperator, UnpackOperator, block_diagonal
+from tamasis.acquisitionmodels import BlackBodyOperator, ConvolutionOperator, CompressionAverageOperator, DdTddOperator, DiscreteDifferenceOperator, DownSamplingOperator, FftHalfComplexOperator, IdentityOperator, InvNttUncorrelatedOperator, InvNttUncorrelatedPythonOperator,  MaskOperator, PackOperator, PadOperator, ConvolutionTruncatedExponentialOperator, RollOperator, ShiftOperator, UnpackOperator, block_diagonal
 from tamasis.numpyutils import all_eq
 
 def test_partitioning():
@@ -223,6 +223,17 @@ def test_ffthalfcomplex3():
     assert np.allclose(b, b_)
     b = fft.T(fft(a))
     assert np.allclose(a, b)
+
+def test_invntt_uncorrelated():
+    filter = np.array([0., 0.2, 0.3, 0.4, 0.4, 0.3, 0.2, 0]).reshape((1,-1))
+    ncorrelations = 2
+    invntt = InvNttUncorrelatedOperator(filter, ncorrelations, 3)
+    invntt_todense = invntt.todense()
+    assert all_eq(invntt_todense, invntt.todense(inplace=True))
+    invntt2 = InvNttUncorrelatedPythonOperator(filter, ncorrelations, 3)
+    invntt2_todense = invntt2.todense()
+    assert all_eq(invntt2_todense, invntt2.todense(inplace=True))
+    assert all_eq(invntt_todense, invntt2_todense)
 
 def test_addition():
     def func(nops):